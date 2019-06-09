56bf380f61aca60f16e0b43a1cd3f5f13a1efaef
Yuriy Provotorov <prowotorow.yuriy@gmail.com>
2019-06-09 17:13:56 +0300

    Add styles for RecipeItem

--- client/src/components/Recipes/RecipeItem/index.js
+++ client/src/components/Recipes/RecipeItem/index.js
@@ -15,41 +15,63 @@ const handleDelete = (deleteUserReipe) => {
 };
 
 const RecipeItem = ({ recipe, withLikes, username }) => (
-  <div className="RecipeItem">
+  <article style={{ boxShadow: '1px 1px 15px 5px rgba(143,143,143,1)' }} className="RecipeItem">
+    <div
+      style={{
+        position: 'relative',
+        background: `url(${recipe.imageUrl}) center center no-repeat`,
+        backgroundSize: 'cover',
+        minHeight: '150px'
+      }}
+    >
+      <p
+        style={{
+          position: 'absolute',
+          bottom: '10px',
+          left: '10px',
+          background: '#00c9b7',
+          borderRadius: '4px',
+          padding: '4px 6px',
+          marginBottom: 0
+        }}
+      >
+        <strong style={{ color: '#ffffff' }}>{recipe.category}</strong>
+      </p>
+      {withLikes && <p>Likes: {recipe.likes}</p>}
+      {username && (
+        <Mutation
+          mutation={DELETE_USER_RECIPE}
+          variables={{ _id: recipe._id }}
+          update={(cache, { data: { deleteUserRecipe } }) => {
+            const { getUserRecipes } = cache.readQuery({
+              query: GET_USER_RECIPES,
+              variables: { username }
+            });
+
+            cache.writeQuery({
+              query: GET_USER_RECIPES,
+              variables: { username },
+              data: {
+                getUserRecipes: getUserRecipes.filter(
+                  (recipe) => recipe._id !== deleteUserRecipe._id
+                )
+              }
+            });
+          }}
+        >
+          {(deleteUserReipe, attr = {}) => (
+            <button className="delete-button" onClick={() => handleDelete(deleteUserReipe)}>
+              {attr.loading ? 'Deleting...' : 'X'}
+            </button>
+          )}
+        </Mutation>
+      )}
+    </div>
+
     <Link to={`/recipes/${recipe._id}`}>
       <h4>{recipe.name}</h4>
     </Link>
-    <p>
-      <strong>{recipe.category}</strong>
-    </p>
-    {withLikes && <p>Likes: {recipe.likes}</p>}
-    {username && (
-      <Mutation
-        mutation={DELETE_USER_RECIPE}
-        variables={{ _id: recipe._id }}
-        update={(cache, { data: { deleteUserRecipe } }) => {
-          const { getUserRecipes } = cache.readQuery({
-            query: GET_USER_RECIPES,
-            variables: { username }
-          });
-
-          cache.writeQuery({
-            query: GET_USER_RECIPES,
-            variables: { username },
-            data: {
-              getUserRecipes: getUserRecipes.filter((recipe) => recipe._id !== deleteUserRecipe._id)
-            }
-          });
-        }}
-      >
-        {(deleteUserReipe, attr = {}) => (
-          <button className="delete-button" onClick={() => handleDelete(deleteUserReipe)}>
-            {attr.loading ? 'Deleting...' : 'X'}
-          </button>
-        )}
-      </Mutation>
-    )}
-  </div>
+  </article>
 );
 
 export default RecipeItem;
